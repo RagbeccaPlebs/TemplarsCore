4cd2172e010f669c0bb32ea77382c26e11881cfb
Rebecca <60572595+RagbeccaPlebs@users.noreply.github.com>
2021-02-23 20:55:37 +0100

    updates

---
+++ src/main/java/com/templarsofmortals/templarscore/common/blocks/StoneBlock.java
@@ -0,0 +1,10 @@
+package com.templarsofmortals.templarscore.common.blocks;
+
+import net.minecraft.block.Block;
+
+public class StoneBlock extends Block {
+
+    public StoneBlock(Properties properties) {
+        super(properties);
+    }
+}
--- src/main/java/com/templarsofmortals/templarscore/core/util/RegistryHelper.java
+++ src/main/java/com/templarsofmortals/templarscore/core/util/RegistryHelper.java
@@ -1,12 +1,14 @@
 package com.templarsofmortals.templarscore.core.util;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import javax.annotation.Nullable;
 import java.util.function.Supplier;
 
 public class RegistryHelper {
@@ -28,12 +30,26 @@ public class RegistryHelper {
         return this.blockRegister;
     }
 
+    /**
+     * Creates an item that has contains a name and supplier
+     * @param name     - Name of the item (String, example: iron_ingot)
+     * @param supplier - The supplier (Way to use: () -> Item(PROPERTIES)
+     * @return Creates an item in the register.
+     */
     public RegistryObject<Item> createItem(String name, Supplier<? extends Item> supplier) {
         return this.itemRegister.register(name, supplier);
     }
 
-    public static Item createSimpleItem(ItemGroup itemGroup) {
-        return new Item(new Item.Properties().group(itemGroup));
+    /**
+     * Creates a block that contains a name, supplier and a item that is in a itemGroup
+     * @param name - Name of the block (String, example: cobblestone)
+     * @param supplier - The supplier (Way to use: () -> new Block)
+     * @param itemGroup - ItemGroup the BlockItem will be stored
+     * @return Creates an block in the register
+     */
+    public RegistryObject<Block> createBlock(String name, Supplier<? extends Block> supplier, ItemGroup itemGroup) {
+        RegistryObject<Block> block = this.blockRegister.register(name, supplier);
+        this.itemRegister.register(name, () -> new BlockItem(block.get(), new Item.Properties().group(itemGroup)));
+        return block;
     }
-
 }
